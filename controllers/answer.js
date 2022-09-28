a777f4816683a8c3984ecd3a5d5cb7871eb0a1d9
alagbe damilare <emmanuel.alagbe@flutterwavego.com>
2022-09-28 21:04:11 +0100

    added try catch to the answer controller functions

--- controllers/answer.js
+++ controllers/answer.js
@@ -3,34 +3,43 @@ const { answerschema } = require("../validation");
 
 // post an answer
 const addAnswer = async (req, res) => {
-  let info = {
-    answer: req.body.answer,
-    user_id: req.user,
-    question_id: req.params.id,
-    upvote: 0,
-    downvote: 0,
-  };
-  const result = await answerschema.validateAsync(info);
-
-  // console.log('result', result)
-  let Answer = await models.answer.create(result);
-  res.status(200).send(Answer);
+  try{
+    let info = {
+      answer: req.body.answer,
+      user_id: req.user,
+      question_id: req.params.id,
+      upvote: 0,
+      downvote: 0,
+    };
+    const result = await answerschema.validateAsync(info);
+  
+    // console.log('result', result)
+    let Answer = await models.answer.create(result);
+    res.status(200).send(Answer);
+  }catch(err){
+    res.status(400).send(err.message)
+  }
 };
 
 
 //get all Answer to a question
 const getAnswers = async (req, res) => {
+  try{
     let question_id = req.params.id;
     let Answers = await models.answer.findAll({
       where: { question_id: question_id },
     });
     res.status(200).send(Answers);
+  }catch(err){
+    res.status(400).send(err.message)
+  }
   };
   
 
 
 // accept an answer
 const acceptanswer = async (req, res) => {
+  try{
     const answer_id = req.params.id;
     const user_id = req.user;
     let answer = await models.answer.findByPk(answer_id);
@@ -47,13 +56,16 @@ const acceptanswer = async (req, res) => {
     if (resultVote[0] < 1) {
       res.status(503).send("server error");
     }
-    // console.log('resultvote',resultVote)
     res.status(204).send();
+  }catch(err){
+    res.status(400).send(err.message)
+  }
   };
   
 
 //upvote an answer 
 const getUpvote = async (req, res) => {
+  try{
     let id = req.params.id;
     const answer = await models.answer.findOne(
       { attributes: ["upvote"] },
@@ -64,11 +76,16 @@ const getUpvote = async (req, res) => {
       { where: { id: id } }
     );
     res.status(200).send();
+  }catch(err){
+    res.status(400).send(err.message)
+  }
+   
   };
 
 
   //downvote an answer 
 const getDownvote = async (req, res) => {
+  try{
     let id = req.params.id;
     const answer = await models.answer.findOne(
       { attributes: ["downvote"] },
@@ -79,6 +96,10 @@ const getDownvote = async (req, res) => {
       { where: { id: id } }
     );
    return res.status(200).send();
+  }catch(err){
+    res.status(400).send(err.message)
+  }
+   
   };
   
 module.exports = {
